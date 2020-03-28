0cd64a9a07eb8461d424dafc866cf0803257b269
Dan Brown <fireflip@gmail.com>
2020-03-28 16:30:18 -0500

    Remove email signup form

--- footer.php
+++ footer.php
@@ -10,14 +10,14 @@
 					</div>
 					<div class="fourcol last txt-align">
 						<div><a href="/employment" class="button employment">Employment</a></div>
-						<form method="post" id="signup" action="https://app.e2ma.net/app2/audience/signup/1773119/1743810/?v=a">
+						<!--form method="post" id="signup" action="https://app.e2ma.net/app2/audience/signup/1773119/1743810/?v=a">
 						<input type="hidden" name="prev_member_email" id="id_prev_member_email">
 						<input type="hidden" name="source" id="id_source">
 						<input type="hidden" name="group_1758146" value="1758146" id="id_group_1758146">
 						<input type="hidden" name="prev_member_email" value="">
 						<input type="email" name="email" id="id_email" required placeholder="Email Updates">
 						<button name="Submit" type="submit" class="button"><i class="fw-right-arrow"></i></button>
-						</form>
+					</form-->
 						<div class="copyright">&copy;<?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?> &bull; Site by <a href="#">The SplitPixel</a></div>
 					</div>
 				</div> <?php // end #inner-footer ?>
